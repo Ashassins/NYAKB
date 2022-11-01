f23ddd7c7791ea1e79b6bed28d11298d831b674b
Ashassins <ashenassassinmc@gmail.com>
2022-11-01 07:05:49 -0400

    remove mini proj on this branch

--- mini_project-main/README.md
+++
@@ -1,3 +0,0 @@
-# ECE 362 Mini Project
-
-Play space invaders on a STM32 using a Wii Nunchuk, a tft display and an audio jack!
--- mini_project-main/inc/timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMER_H_
-#define TIMER_H_
-
-int glbcnt;
-
-void init_tim6();
-void TIM6_DAC_IRQHandler(void);
-
-#endif
